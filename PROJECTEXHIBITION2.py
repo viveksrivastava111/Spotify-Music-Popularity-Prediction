50a3b2c2b5ef57d1d0ca70ff94af4c6280368333
Vivek Srivastava <155307110+viveksrivastava111@users.noreply.github.com>
2025-10-24 17:44:39 +0530

    Update PROJECTEXHIBITION2.py

--- PROJECTEXHIBITION2.py
+++ PROJECTEXHIBITION2.py
@@ -14,6 +14,7 @@ from sklearn.preprocessing import PolynomialFeatures, StandardScaler, QuantileTr
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import Ridge
 
+
 FILE_PATH = r"C:\Users\LENOVO\Desktop\SpotifyFeatures.csv"
 @st.cache_data
 def load_data():
@@ -253,6 +254,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
