229d224b996965e9460a0cc4eee139b9c5f7e1f3
Vivek Srivastava <155307110+viveksrivastava111@users.noreply.github.com>
2026-03-09 23:50:18 +0530

    Update PROJECTEXHIBITION2.py

--- PROJECTEXHIBITION2.py
+++ PROJECTEXHIBITION2.py
@@ -15,6 +15,11 @@ from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import Ridge
 
 
+def load_dataset():
+    df = load_data()
+    if df is None:
+        return
+
 FILE_PATH = r"C:\Users\LENOVO\Desktop\SpotifyFeatures.csv"
 @st.cache_data
 def load_data():
@@ -253,6 +258,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
